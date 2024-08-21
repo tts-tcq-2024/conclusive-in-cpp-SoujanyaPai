9054658e9331f32de103b639b8d9862ccaa6b940
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-21 21:32:42 +0530

    Create CoolingStrategy.hpp

---
+++ CoolingStrategy.h
@@ -0,0 +1,34 @@
+#ifndef COOLING_STRATEGY_HPP
+#define COOLING_STRATEGY_HPP
+
+#include "typewise-alert.hpp"
+#include "BreachType.hpp"
+
+class CoolingStrategy {
+public:
+    virtual ~CoolingStrategy() = default;
+    virtual BreachType classifyTemperature(double temperatureInC) const = 0;
+};
+
+class HiActiveCoolingStrategy : public CoolingStrategy {
+public:
+    BreachType classifyTemperature(double temperatureInC) const override {
+        return inferBreach(temperatureInC, 0, 45);
+    }
+};
+
+class MedActiveCoolingStrategy : public CoolingStrategy {
+public:
+    BreachType classifyTemperature(double temperatureInC) const override {
+        return inferBreach(temperatureInC, 0, 40);
+    }
+};
+
+class PassiveCoolingStrategy : public CoolingStrategy {
+public:
+    BreachType classifyTemperature(double temperatureInC) const override {
+        return inferBreach(temperatureInC, 0, 35);
+    }
+};
+
+#endif // COOLING_STRATEGY_HPP
