5aea54b6cb7591f15e6280c0431b70ac22f2316b
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-22 01:14:12 +0530

    Update typewise-alert.tests.cpp

--- typewise-alert.tests.cpp
+++ typewise-alert.tests.cpp
@@ -2,6 +2,24 @@
 #include "typewise-alert.h"
 
 TEST(TypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
+    BatteryCharacter batteryChar;
+    PassiveCoolingStrategy passiveStrategy;
+    HiActiveCoolingStrategy hiActiveStrategy;
+    MedActiveCoolingStrategy medActiveStrategy;
 
+    batteryChar.coolingStrategy = &passiveStrategy;
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, -5), TOO_LOW);
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, 39), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, 20), NORMAL);
+
+    batteryChar.coolingStrategy = &hiActiveStrategy;
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, -1), TOO_LOW);
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, 55), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, 44), NORMAL);
+
+    batteryChar.coolingStrategy = &medActiveStrategy;
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, -3), TOO_LOW);
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, 50), TOO_HIGH);
+    EXPECT_EQ(classifyTemperatureBreach(batteryChar, 33), NORMAL);
 
 }
