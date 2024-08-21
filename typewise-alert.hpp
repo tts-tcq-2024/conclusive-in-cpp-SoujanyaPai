d56819306f5e4c1f37aa059a82e12444023e0654
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-21 21:40:14 +0530

    Update and rename typewise-alert.h to typewise-alert.hpp

---
+++ typewise-alert.hpp
@@ -0,0 +1,30 @@
+#ifndef TYPEWISEALERT_HPP
+#define TYPEWISEALERT_HPP
+
+#include "CoolingStrategy.hpp"
+#include "BatteryCharacter.hpp"
+#include <unordered_map>
+
+typedef enum {
+  PASSIVE_COOLING,
+  HI_ACTIVE_COOLING,
+  MED_ACTIVE_COOLING
+} CoolingType;
+
+typedef enum {
+  TO_CONTROLLER,
+  TO_EMAIL
+} AlertTarget;
+
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
+
+void checkAndAlert(
+  AlertTarget alertTarget, const BatteryCharacter batteryChar, double temperatureInC);
+
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
+
+typedef void (*AlertFunction)(BreachType);
+
+#endif //TYPEWISE-ALERT_HPP
