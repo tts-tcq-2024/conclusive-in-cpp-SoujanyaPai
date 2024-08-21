fe63c1dc5554b9e208f8ec55ce826897ae004fb9
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-22 03:06:05 +0530

    Update coolingStrategy.hpp

--- coolingStrategy.hpp
+++ coolingStrategy.hpp
@@ -10,4 +10,25 @@ public:
     virtual BreachType classifyTemperature(double temperatureInCelcius) const = 0;
 };
 
+class HiActiveCoolingStrategy : public CoolingStrategy {
+public:
+    BreachType classifyTemperature(double temperatureInCelcius) const override {
+        return inferBreach(temperatureInCelcius, 0, 45);
+    }
+};
+
+class MedActiveCoolingStrategy : public CoolingStrategy {
+public:
+    BreachType classifyTemperature(double temperatureInCelcius) const override {
+        return inferBreach(temperatureInCelcius, 0, 40);
+    }
+};
+
+class PassiveCoolingStrategy : public CoolingStrategy {
+public:
+    BreachType classifyTemperature(double temperatureInCelcius) const override {
+        return inferBreach(temperatureInCelcius, 0, 35);
+    }
+};
+
 #endif // COOLING_STRATEGY_HPP
