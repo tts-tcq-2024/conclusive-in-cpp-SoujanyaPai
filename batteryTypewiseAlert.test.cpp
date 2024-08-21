3d21b6d2135f8986e2a49b00b512709ed288d1e1
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-22 03:08:23 +0530

    Update batteryTypewiseAlert.test.cpp

--- batteryTypewiseAlert.test.cpp
+++ batteryTypewiseAlert.test.cpp
@@ -4,21 +4,21 @@
 
 TEST(BatteryTypeWiseAlertTestSuite,InfersBreachAccordingToLimits) {
     BatteryCharacter batteryChar;
-    PassiveCoolingStrategy passiveStrategy;
-    HiActiveCoolingStrategy hiActiveStrategy;
-    MedActiveCoolingStrategy medActiveStrategy;
+    PassiveCoolingStrategy passiveCooling;
+    HiActiveCoolingStrategy hiActiveCooling;
+    MedActiveCoolingStrategy medActiveCooling;
 
-    batteryChar.coolingStrategy = &passiveStrategy;
+    batteryChar.coolingStrategy = &passiveCooling;
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, -5), TOO_LOW);
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, 39), TOO_HIGH);
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, 20), NORMAL);
 
-    batteryChar.coolingStrategy = &hiActiveStrategy;
+    batteryChar.coolingStrategy = &hiActiveCooling;
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, -1), TOO_LOW);
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, 55), TOO_HIGH);
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, 44), NORMAL);
 
-    batteryChar.coolingStrategy = &medActiveStrategy;
+    batteryChar.coolingStrategy = &medActiveCooling;
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, -3), TOO_LOW);
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, 50), TOO_HIGH);
     EXPECT_EQ(classifyTemperatureBreach(batteryChar, 33), NORMAL);
