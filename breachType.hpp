07844a3df23cd752a75700783a36eaad29c0e58e
SoujanyaPai <64064266+SoujanyaPai@users.noreply.github.com>
2024-08-22 10:40:10 +0530

    Update breachType.hpp

--- breachType.hpp
+++ breachType.hpp
@@ -1,8 +1,6 @@
 #ifndef BREACH_TYPE_HPP
 #define BREACH_TYPE_HPP
 
-#include "batteryCharacter.hpp"
-
 enum BreachType {
     NORMAL,
     TOO_LOW,
@@ -10,6 +8,5 @@ enum BreachType {
 };
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(const BatteryCharacter& batteryChar, double temperatureInCelcius);
 
 #endif // BREACHTYPE_HPP
